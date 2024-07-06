617461efcdaad036fc799a6a8cee15336fcdf843
castlehunter <vvxiaobai@gmail.com>
2024-07-05 23:15:53 -0400

    c

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import "./App.css";
 
 function App() {
+  function reducer(state, action) {
+    switch (action.type) {
+      case "start":
+        return { ...state, status: "active" };
+      case "dataReceived":
+        return { ...state, data: action.payload };
+      default:
+        throw new Error("Action unknown");
+    }
+  }
+
+  const initialState = { status: "loading", data: [] };
+  const [{ status, data }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      {status === "loading" && <StartScreen dispatch={dispatch} />}
+      {status === "active" && <Question data={data} />}
     </div>
   );
+}
+
+function Logo() {
+  return <h1 style={{ textAlign: "center" }}>REACT QUIZ</h1>;
+}
+
+function StartScreen({ dispatch }) {
+  return (
+    <div>
+      <h2>Welcome</h2>
+      <button onClick={() => dispatch({ type: "start" })}>Start</button>
+    </div>
+  );
+}
+
+function Question({ data }) {
+  return <div>{data.map((d) => d.question)}</div>;
 }
 
 export default App;
